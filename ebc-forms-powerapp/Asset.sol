ce1d339e44969204c0ef7c841cc5a5f8ea261086
David Smith <25447681+davidcozsmith@users.noreply.github.com>
2019-04-20 07:12:07 -0700

    fixing asset.sol

--- ebc-forms-powerapp/Asset.sol
+++ ebc-forms-powerapp/Asset.sol
@@ -1,69 +1,76 @@
 pragma solidity ^0.5.0;
-
 contract Asset
 {
-
-  enum StateType { CanBeReserved, Reserved }
+  enum StateType { CanBeReserved, ReservationRequested, ReservationApproved }
 
   AssetDetails public asset;
 
-  struct AssetDetails {
-    StateType State;
-    string Description;
-    address Owner;
-    address ReservedBy;
-    uint ReservedOn;
-    uint ReservedDays;
-    uint Longitude;
-    uint Latitude;
-  }
-
   event AssetCreated();
-  event ReservationGranted(uint reservedDays, uint latitude, uint longitude);
+  event ReservationRequested(address requestedBy, uint reservedDays, uint latitude, uint longitude);
+  event ReservationGranted();
   event ReservationReleased();
 
-  constructor (string memory description) public
-	{
-    asset = AssetDetails({
-      Description: description,
-      Owner: address(msg.sender),
-      ReservedBy: address(0x0),
-      ReservedOn: 0,
-      ReservedDays: 0,
-      Longitude: 0,
-      Latitude: 0,
-      State: StateType.CanBeReserved
-    });
-    emit AssetCreated();
+  struct AssetDetails {
+    string Description;
+    uint Latitude;
+    uint Longitude;
+    address Owner;
+    address RequestedBy;
+    uint ReservedDays;
+    uint ReservedOn;
+    StateType State;
   }
 
   function estimateNow() private view returns (uint) { return (block.number * 15 seconds); }
   function expiresOn() private view returns (uint) { return (asset.ReservedOn + (asset.ReservedDays * 1 days)); }
 
-  function Reserve(address reservedBy, uint reservedDays, uint longitude, uint latitude) public
+  constructor (string memory description, address owner) public
 	{
-    require(asset.Owner == msg.sender, "Only the current owner can assign a new reservation" );
-    require(asset.State == StateType.CanBeReserved, "The asset is already reserved");
+    asset = AssetDetails({
+      Description: description,
+      Latitude: 0,
+      Longitude: 0,
+      Owner: owner,
+      RequestedBy: address(0x0),
+      ReservedDays: 0,
+      ReservedOn: 0,
+      State: StateType.CanBeReserved
+    });
+    emit AssetCreated();
+  }
 
-    asset.State = StateType.Reserved;
-    asset.ReservedBy = reservedBy;
-    asset.ReservedOn = estimateNow();
+  function RequestReservation(address requestedBy, uint reservedDays, uint longitude, uint latitude) public
+	{
+    require(asset.State == StateType.CanBeReserved, "The asset is already reserved");
+    asset.State = StateType.ReservationRequested;
+    asset.RequestedBy = requestedBy;
     asset.ReservedDays = reservedDays;
     asset.Longitude = longitude;
     asset.Latitude = latitude;
-    emit ReservationGranted(reservedDays, longitude, latitude);
+    emit ReservationRequested(requestedBy, reservedDays, longitude, latitude);
+  }
+
+  function ApproveReservation() public
+	{
+    require(
+      asset.Owner == msg.sender,
+      "Only the owner can approve the reservation request"
+    );
+    require(asset.State == StateType.ReservationRequested, "The asset is already reserved");
+
+    asset.State = StateType.ReservationApproved;
+    asset.ReservedOn = estimateNow();
+    emit ReservationGranted();
   }
 
   function ReleaseReservation() public
 	{
-    require(asset.State == StateType.Reserved, "Not currently reserved");
-    require(
-      asset.ReservedBy == msg.sender || estimateNow() > expiresOn(),
-      "If the reservation is not expired, only the reservation owner can release it"
-    );
+    require(asset.State == StateType.ReservationApproved, "Not reserved");
+    bool isExpired = estimateNow() > expiresOn();
+    require(asset.RequestedBy == msg.sender || isExpired, "If the reservation is not expired, only the reservation requestor can release it");
 
     asset.State = StateType.CanBeReserved;
-    asset.ReservedBy = address(0x0);
+    asset.RequestedBy = address(0x0);
     asset.ReservedOn = 0;
     asset.ReservedDays = 0;
     asset.Longitude = 0;
