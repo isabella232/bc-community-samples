fa8f882d8f7fe2941db7645f3b46876158e488ea
stejin <steffen@stejin.org>
2019-04-29 09:32:47 -0700

    Fixed address

--- weather-insurance-app/stejin/WebApp/src/components/NetworkError.vue
+++ weather-insurance-app/stejin/WebApp/src/components/NetworkError.vue
@@ -2,6 +2,6 @@
   <div>
     <p>Please use a web3 browser or install MetaMask</p>
     <p>For best user experience run a local node with unlocked account</p>
-    <p>parity --chain ropsten --jsonrpc-cors https://prod.proderivatives.com --unlock 0x...</p>
+    <p>parity --chain ropsten --jsonrpc-cors https://stejin.z22.web.core.windows.net --unlock 0x...</p>
   </div>
 </template>
